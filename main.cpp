18827e9204e444852b03e0a2d47301cd7aaa5e50
lucasribeiroabreu <lucasribeiroabreu@gmail.com>
2018-07-30 23:18:49 -0300

    Alteracoes grandes dfs bfs

--- main.cpp
+++ main.cpp
@@ -4,82 +4,96 @@
 #include<list>
 #include<string>
 
-#define NULL_CELL -1
-#define CELULA_INVALIDA -2
-#define BOARD_SIZE 3
+#define CELULA_NULA -1  // CELULA QUE É POSSIVEL MOVIMENTO 
+#define CELULA_INVALIDA -2  // CELULA QUE É IMPOSSIVEL MOVIMENTO 
+#define BOARD_SIZE 3  //LARGURA DO TABULEIRO QUADRADO
 
 using namespace std;
-    // g++ main.cpp -o main && ./main
-
-    // posicoes
-    //  0 1 2
-    //  3 4 5
-    //  6 7 8
-
-    // movimentos
-    // n - e b d c (anti horario)
-    // 0 -   3 1
-    // 1 - 0 4 2 
-    // 2 - 1 5 
-    // 3 -   6 4 0
-    // 4 - 3 7 5 1
-    // 5 - 4 8
-    // 6 -     7 3
-    // 7 - 6   8 4
-    // 8 - 7     5
 
 class Cell{
     public:
         int i, valor;
-        int posicoes[4];
+        int vizinhos[4];
         
     Cell(){}
     Cell(int i, int valor): i(i), valor(valor){}
+    
+    bool isnull(){
+        // CELULA QUE É POSSIVEL MOVIMENTO 
+        return valor == CELULA_NULA;
+    }
+    
+    bool isValidCell(int i){
+        // CELULA QUE É IMPOSSIVEL MOVIMENTO 
+        return vizinhos[i] != CELULA_INVALIDA;
+    }
+
+    string toString(){
+        // MONTA STRING QUE REPRESENTA A CELULA
+        return "Cell[" +  to_string(i) + "] = " + to_string(valor);
+    }
 
     void imprime(){
         cout << toString() << endl;
     }  
 
-    string toString(){
-        return "Cell[" +  to_string(i) + "] = " + to_string(valor);
-    }
+    void getPossibleSwaps(){
+        // verifica pra quais celulas vizinhas (cima, baixo, direita e esquerda) é possível fazer a troca
+        // BOARD_SIZE = 3;
+        // posicoes
+        //  0 1 2
+        //  3 4 5
+        //  6 7 8
 
-    void get_possible_swaps(){
-        if (i % BOARD_SIZE == 0) posicoes[0] = CELULA_INVALIDA;
-        else posicoes[0] = i - 1;
+        if (i % BOARD_SIZE == 0) vizinhos[0] = CELULA_INVALIDA;
+        else vizinhos[0] = i - 1;
 
-        if ((i+1) % BOARD_SIZE == 0) posicoes[1] = CELULA_INVALIDA;
-        else posicoes[1] = i + 1;
+        if ((i+1) % BOARD_SIZE == 0) vizinhos[1] = CELULA_INVALIDA;
+        else vizinhos[1] = i + 1;
 
-        if ((i + BOARD_SIZE) >= BOARD_SIZE*BOARD_SIZE) posicoes[2] = CELULA_INVALIDA;
-        else posicoes[2] = i + BOARD_SIZE;
+        if ((i + BOARD_SIZE) >= BOARD_SIZE*BOARD_SIZE) vizinhos[2] = CELULA_INVALIDA;
+        else vizinhos[2] = i + BOARD_SIZE;
 
-        if ((i - BOARD_SIZE) < 0) posicoes[3] = CELULA_INVALIDA;
-        else posicoes[3] = i - BOARD_SIZE;
-
-        // cout << "Cell[" << i << "] = " << valor << " | E:" << posicoes[0] << " B:" << posicoes[1] << " D:"<< posicoes[2] << " C:" << posicoes[3] << endl;
+        if ((i - BOARD_SIZE) < 0) vizinhos[3] = CELULA_INVALIDA;
+        else vizinhos[3] = i - BOARD_SIZE;
     }
 };
 
 class Board{
     public:
         Cell board[9];
-        Cell nc;
+        Cell celulaNula;
         Board(){}
+        Board(string tipoInicializacao){
+            if (tipoInicializacao == "fix"){
+                initializeFixBoard();
+            }
+            else if(tipoInicializacao == "target"){
+                initializeTargetBoard();
+            }
+            else{
+                initializeRandomBoard();
+                cout << "Tabuleiro iniciado aleatoriamente" << endl;
+            }
+        }
 
-        void initialize_target_board(){
+        bool checaMovimentoValido(int i){
+            return celulaNula.isValidCell(i);
+        }
+
+        void initializeTargetBoard(){
             // target
             //  1 | 2 | 3
             //  4 | 5 | 6
             //  7 | 8 | -
             int n = 1;
             for(int i=0; i<BOARD_SIZE*BOARD_SIZE; i++){
-                if(i==8) board[i] = Cell(i, NULL_CELL);
+                if(i==8) board[i] = Cell(i, CELULA_NULA);
                 else board[i] = Cell(i, n);
                 n++;
             }
         }
-        void initialize_fix_board(){
+        void initializeFixBoard(){
             // | 1 | 2 | 3
             // | 7 | 4 | 5
             // | 8 | - | 6
@@ -90,16 +104,16 @@ class Board{
             board[4] = Cell(4, 4);
             board[5] = Cell(5, 5);
             board[6] = Cell(6, 8);
-            board[7] = Cell(7, NULL_CELL);
+            board[7] = Cell(7, CELULA_NULA);
             board[8] = Cell(8, 6);
         }
 
-        void initialize_random_board(){
+        void initializeRandomBoard(){
             int n = 1;
             int nullcell_pos = rand() % (BOARD_SIZE*BOARD_SIZE);
 
             for(int i=0; i<BOARD_SIZE*BOARD_SIZE; i++){
-                if(i==nullcell_pos) board[i] = Cell(i, NULL_CELL);
+                if(i==nullcell_pos) board[i] = Cell(i, CELULA_NULA);
                 else {
                     board[i] = Cell(i, n);
                     n++;
@@ -109,33 +123,33 @@ class Board{
 
         string toString(){
             string s = "";
-            for(int i=0; i<BOARD_SIZE*BOARD_SIZE; i++) 
-                s += "|" + board[i].toString();
+            for(int i=0; i<BOARD_SIZE*BOARD_SIZE; i++){
+                if (board[i].isnull()) s +=  " | -";
+                else s += " | " + to_string(board[i].valor);
+                if (((i+1) % BOARD_SIZE) == 0) s += "\n";
+            }
             return s;
         }
+        
+        void printBoard(){
+            cout << toString() << endl;
+        }
 
-        void get_null_cell(){
+        void localizaCelulaNula(){
             for(int i=0; i<BOARD_SIZE*BOARD_SIZE; i++)
-                if (board[i].valor == NULL_CELL){
-                    nc = board[i];
-                    nc.get_possible_swaps();
+                if (board[i].isnull()){
+                    celulaNula = board[i];
+                    celulaNula.getPossibleSwaps();
                 }
         }
 
-        void print_board(){
-            for(int i=0; i<BOARD_SIZE*BOARD_SIZE; i++){
-                if (board[i].valor == NULL_CELL) cout << " | " << "-";
-                else cout << " | " << board[i].valor;
-                if (((i+1) % 3) == 0) cout << endl;
-            }
-            cout << endl;
-        }
         bool operator==(const Board& b){
             for(int i=0; i<BOARD_SIZE*BOARD_SIZE; i++)
                 if (board[i].valor != b.board[i].valor) 
                     return false;
             return true;
         }
+
         void swap(int pos1, int pos2){
             Cell swp = board[pos2];
             swp.i = pos1;
@@ -150,13 +164,18 @@ class Board{
 
 class Solver{
     public:
-        int count;
+        int count, movimentos;
         Board solution;
-        map<string, bool> cache_map;
+        map<string, string> cache_map;
+        Board target;
 
-        Solver(): count(0){}
+        Solver(): count(0), movimentos(0) {
+            // Inicializa Target
+            target = Board("target");    
+        }
 
-        bool check_end(Board& target, Board& b){
+        bool checkFim(Board& b){
+            // Checa se o board recebido é o target desejado
             if (b==target){
                 solution = b;
                 cout << "Solved! " << count << " iterations." << endl;
@@ -164,34 +183,57 @@ class Solver{
             }
             return false;
         }
-        bool in_cache(Board b){
+        bool inCache(Board& b, Board& pai){
+            // Checa se o board recebido já foi visitado (está no cache). Se nao, guarda no cache o board e o Pai no cache 
             string s = b.toString();
-            if (!cache_map[s]){
-                cache_map[s] = true;
+
+            if (cache_map[s]==""){
+                cache_map[s] = pai.toString();
                 return false;
             }
             return true;
         }
         
-        bool solve_board_bfs(Board& target, Board b, bool use_bfs){
-            if(use_bfs) cout << "BFS: " ;
+        void showSolution(Board inicial, bool printSolution){
+            string repr = solution.toString();
+            
+            if (printSolution)
+                cout << endl << "Partindo do target ate o estado inicial" << endl << "=== Printing solution ===" << endl << repr << endl;
+                
+            int c = 0;
+            while (true){
+                if (repr==inicial.toString()) {
+                    cout << "FIM -- " << c << " movimentos." << endl;
+                    movimentos = c;
+                    break;
+                }
+                c++;
+                string pai_repr = cache_map[repr];
+                
+                if (printSolution)
+                    cout << pai_repr << endl;
+                
+                repr = pai_repr;
+            }
+        }
+
+        bool solveBoardBFS(Board b, bool useBFS){
+            if(useBFS) cout << "BFS: " ;
             else cout << "DFS: " ;
 
+            // Checa se ja está no target e adiciona ao cache
+            if (checkFim(b) || inCache(b, b)) return true;
+
+            // Add a fila/pilha dependendo do argumento use_bfs
             list<Board> F;
-            Board board;
-            
-            // Check end
-            if (check_end(target, b)) return true;
-            
-            // Check cache
-            in_cache(b);
-            
-            // Add to tree
             F.push_back(b);
 
+            // Enquanto a fila/pilha nao esta vazia
+            Board board;
             while(!F.empty()){
-
-                if(use_bfs){
+                
+                // Coleta um novo "No" da fila/pilha
+                if(useBFS){
                     board = F.front();
                     F.pop_front();
                 }
@@ -200,33 +242,32 @@ class Solver{
                     F.pop_back();
                 }
              
-                // Check iterations
+                // Checa iteracoes
                 count++;
-                if (count > 500000){
+                if (count > 150000){
                     solution = board;
-                    cout << "Ending after 500k iterations" << endl;
+                    cout << "Parando execucao apos 150k nós gerados" << endl;
                     break;
                 }
                 
-                // Get current null cell
-                board.get_null_cell();
+                // Localiza celula vazia
+                board.localizaCelulaNula();
 
-                // For each available move (up down left right)
+                // Para cada posicao vazia em torno da celula vazia
                 for (int i=0; i<4; i++){
-                    if (board.nc.posicoes[i] != CELULA_INVALIDA){
-                        Board tmp = board;
+                    if (board.checaMovimentoValido(i)){
+                        // Troca celula nula com o vizinho com movimento valido
+                        Board nextBoard = board;
+                        nextBoard.swap(nextBoard.celulaNula.i, nextBoard.celulaNula.vizinhos[i]);
                         
-                        // Swap null with choice
-                        tmp.swap(tmp.nc.i, tmp.nc.posicoes[i]);
+                        // Checa cache
+                        if (inCache(nextBoard, board)) continue;
+
+                        // Checa fim
+                        if (checkFim(nextBoard)) return true;
                         
-                        // Check cache
-                        if (in_cache(tmp)) continue;
-
-                        // Check end
-                        if (check_end(target, tmp)) return true;
-
-                        // Add to tree
-                        F.push_back(tmp);
+                        // Adiciona a arvore
+                        F.push_back(nextBoard);
                     }
                 }
             }
@@ -238,26 +279,16 @@ class Solver{
 int main(int argc, char const *argv[])
 {
     srand(time(NULL));
-
-    // Puzzle Inicial
-    cout << "Puzzle Inicial" << endl;
-    Board b = Board();    
-    b.initialize_random_board();
-    b.print_board();
-
-    // Target
-    Board target = Board();    
-    target.initialize_target_board();
-    target.print_board();
-
-    cout << target.toString() << endl;
-    // BFS
+    Board b = Board("random"); //random, fix or target
+    
     Solver s;
-    s.solve_board_bfs(target, b, true);
+    s.solveBoardBFS(b, true);  //true BFS
+    s.showSolution(b, false);
 
     // DFS
-    Solver s2;
-    s2.solve_board_bfs(target, b, false);
-    // s.solution.print_board();
+    s = Solver();
+    s.solveBoardBFS(b, false); //false DFS
+    s.showSolution(b, false);
 
+   
 }   
